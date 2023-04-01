011da8f440f91b6b9059f4d164555d439a949e6c
hypnojerk <hypnojerk@users.noreply.github.com>
2023-03-31 18:41:51 -0700

    🐛 fix(chat.ts): make ChatAi function asynchronous and handle errors ✨ feat(chat.ts): update chat object with returned data from OpenAI API The ChatAi function is now asynchronous to handle the API call to OpenAI. The function now also handles errors that may occur during the API call. The chat object is updated with the returned data from the OpenAI API.

--- src/modules/context/chat.ts
+++ src/modules/context/chat.ts
@@ -1,23 +1,29 @@
 import { MessageList, Chat, Message } from '../../types/chat'
 import { API } from '../../modules/api/server'
 
-const ChatAi = (chat: Chat) => {
+const ChatAi = async (chat: Chat) => {
   // TODO: Implement chat AI
   // Use the chatId to get the messages from the database
   // also get the promptId and temperature
   // set prompt with promptId
   // set temperature with temperature
-  const api = API()
+  const api = await API()
   const { chatId, messages, promptId, prompt, temperature } = chat
 
-  const returnedData = api.chat({
-    chatId,
-    messages,
-    promptId,
-    prompt,
-    temperature,
-  })
-
+  try {
+    const returnedData = await api.chat({
+      chatId,
+      messages,
+      promptId,
+      prompt,
+      temperature,
+    })
+    const returnedChat: Chat = returnedData.request.data
+    chat = returnedChat
+  } catch (error) {
+    console.error('Error while communicating with OpenAI API:', error)
+    throw error
+  }
   return chat
 }
 
